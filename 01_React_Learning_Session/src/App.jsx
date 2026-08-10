9712a54e3f6acad814ef58dd72bcd829b901477c
gauravsri26 <srivastavagaurav2605@gmail.com>
2026-08-10 10:52:33 +0530

    Learned about React folder structure and about files in it

---
+++ 01_React_Learning_Session/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Heloo = () => {
+  return (
+    <>
+      <div id='Papa'>
+        <h1 id='Child01'>Hello Everyone Good Morning</h1>
+        <h2 id='Child02'> - My Name is Gaurav</h2>
+      </div>
+      <div id="chacha">
+        <h1 id='chahcha_child01'>Hey i am yash</h1>
+      </div>
+    </>
+  )
+}
+
+export default Heloo
